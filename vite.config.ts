37fa0f77c57a74dca539f60b00b56583a8b60e7a
freeaitoken <freeaitoken@gmail.com>
2026-06-24 16:02:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
-
+import { defineConfig } from 'vite'
+export default defineConfig({
+  base: '/anvoria-arc/'
+})
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
